6f4fa3701eec98097970f5184a0331ca43b8bf47
LucasNun3z <lucanunez251@gmail.com>
2025-06-12 21:43:25 -0300

    Configurar GitHub Pages y despliegue automático

--- rick-and-morty-vue/vite.config.js
+++ rick-and-morty-vue/vite.config.js
@@ -1,22 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath } from 'url'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],  resolve: {
+  base: '/rick-and-morty-vue/',
+  plugins: [vue()],
+  resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'vue': 'vue/dist/vue.esm-bundler.js'
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
-  optimizeDeps: {
-    include: ['ag-grid-vue3', 'ag-grid-community']
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/assets/global.css";`
-      }
-    }
+  test: {
+    globals: true,
+    environment: 'jsdom'
   }
 })
